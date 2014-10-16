22d3da1dc32c3e12763332bf6b63e0f3081c9ee9
Todd Sifleet <todd.sifleet@gmail.com>
2014-10-16 08:40:24 -0700

    reorganize __init__

--- fuzzy_match/__init__.py
+++ fuzzy_match/__init__.py
@@ -1,7 +1,16 @@
-from expecter import Expecter as expect  # noqa
 from string_matchers import StringContainsMatcher as contains  # noqa
 from string_matchers import StringStartsWithMatcher as startswith  # noqa
 from string_matchers import StringEndsWithMatcher as endswith  # noqa
 from string_matchers import StringRegexMatcher as matches  # noqa
-from matchers import same_elements  # noqa
-from matchers import includes_elements  # noqa
+
+from delegators import same_elements  # noqa
+from delegators import includes_elements  # noqa
+
+from object_matchers import MatchAnything
+from object_matchers import MatchAnythingTruthy
+from object_matchers import MatchAnythingFalsey
+from object_matchers import MatchAnyInstanceOf as any_instance  # noqa
+
+anything = MatchAnything()
+anything_false = MatchAnythingFalsey()
+anything_true = MatchAnythingTruthy()
