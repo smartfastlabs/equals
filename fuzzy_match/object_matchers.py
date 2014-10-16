be1370d445422bba8bc1c6a05863deceb9004758
Todd Sifleet <todd.sifleet@gmail.com>
2014-10-16 08:39:50 -0700

    add object matchers

---
+++ fuzzy_match/object_matchers.py
@@ -0,0 +1,24 @@
+class Matcher(object):
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+
+class MatchAnything(Matcher):
+    def __eq__(self, value):
+        return True
+
+
+class MatchAnythingTruthy(Matcher):
+    def __eq__(self, value):
+        return bool(value)
+
+
+class MatchAnythingFalsey(Matcher):
+    def __eq__(self, value):
+        return not bool(value)
+
+
+class MatchAnyInstanceOf(Matcher):
+    def __eq__(self, value):
+        return isinstance(value, *self.args)
---
+++ test/object_test.py
@@ -0,0 +1,33 @@
+from fuzzy_match import anything, anything_true, anything_false, any_instance
+
+
+def test_anything_matches_none():
+    assert anything == None  # noqa
+
+
+def test_anything_matches_true():
+    assert anything == True  # noqa
+
+
+def test_anything_true_matches_true():
+    assert anything_true == True  # noqa
+
+
+def test_anything_true_matches_a_string():
+    assert anything_true == 'test'
+
+
+def test_anything_true_does_not_match_false():
+    assert not anything_true == False  # noqa
+
+
+def test_anything_false_matches_false():
+    assert anything_false == False  # noqa
+
+
+def test_anything_false_does_not_match_true():
+    assert not anything_false == True  # noqa
+
+
+def test_any_instance_matches():
+    assert any_instance(dict) == {}
