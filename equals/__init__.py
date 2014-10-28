dc26f4155538c0f7c120b81e6af4a247a0375aef
Todd Sifleet <todd.sifleet@gmail.com>
2014-10-28 08:26:35 -0700

    update setup.py/setup.cfg

--- equals/__init__.py
+++ equals/__init__.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import
 
+__version__ = '0.0.2'
+
 import numbers
 import collections
 
--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from distutils.core import setup
+
+import equals
+
+with open('README.rst') as f:
+    long_description = f.read()
+
 setup(
-    description = 'Python Fuzzy Matchers',
-    name =  'equals',
-    version = '0.0.01',
+    description='Python Fuzzy Matchers',
+    long_description=long_description,
+    name='equals',
+    version=equals.__version__,
     author='Todd Siflet',
     author_email='todd.siflet@gmail.com',
     py_modules = [],
