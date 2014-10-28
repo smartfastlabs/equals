99ee728c1df3113330d00063a7119423e972b614
Todd Sifleet <todd.sifleet@gmail.com>
2014-10-27 19:59:54 -0700

    update dict test for args and kwargs

--- test/dictionary_test.py
+++ test/dictionary_test.py
@@ -40,6 +40,42 @@ class TestContainingKeys(object):
     def test_fails_on_dict_with_missing_keys(self):
         assert not self.test_obj == {'bob': 'barker'}
 
+    def test_representation(self):
+        expected = "Any instance of <(type|class) 'dict'> containing: foo, bob"
+        assert re.match(expected, str(self.test_obj))
+        assert re.match('<Equals {}>'.format(expected), repr(self.test_obj))
+
+
+class TestContaingKeysAndItems(object):
+    test_obj = any_dict.containing('foo', 'bar', bob='barker')
+
+    def test_passes_on_dict_with_keys_and_items(self):
+        assert self.test_obj == {
+            'foo': 'bar',
+            'bob': 'barker',
+            'bar': 'bobby',
+        }
+
+    def test_fails_on_dict_with_keys_but_not_items(self):
+        assert not self.test_obj == {
+            'foo': 'bar',
+            'bar': 'bobby',
+        }
+
+    def test_fails_on_dict_with_items_but_not_all_keys(self):
+        assert not self.test_obj == {
+            'foo': 'bar',
+            'bob': 'barker',
+        }
+
+    def test_representation(self):
+        expected = (
+            "Any instance of <(type|class) 'dict'> containing: "
+            "foo, bar and bob=barker"
+        )
+        assert re.match(expected, str(self.test_obj))
+        assert re.match('<Equals {}>'.format(expected), repr(self.test_obj))
+
 
 def test_order_of_test_does_not_matter():
     assert {'foo': 'bar'} == any_dict.containing(foo='bar')
