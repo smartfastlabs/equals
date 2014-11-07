366f11255e6a0fb1b71fd052f6a7dca77eed72d9
Todd Sifleet <todd.sifleet@gmail.com>
2014-11-07 06:53:31 -0800

    Add new files required for coverage

---
+++ test/conftest.py
@@ -0,0 +1,14 @@
+from coverage import coverage
+
+cov = coverage(source=('equals',))
+cov.start()
+
+
+def pytest_sessionfinish(session, exitstatus):
+    cov.stop()
+    cov.save()
+
+
+def pytest_terminal_summary(terminalreporter):
+    print "\nCoverage report:\n"
+    cov.report(show_missing=True, ignore_errors=True, file=terminalreporter._tw)
